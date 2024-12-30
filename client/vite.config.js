1a8e5e4b0927e5c8f7fd1c6a552aaadc6ba6f782
Tarango555 <tarango.aiub@gmail.com>
2024-12-31 01:36:20 +0600

    second

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['jwt-decode'], // Externalize the jwt-decode package
+    }
+  },
   plugins: [react()],
   server: {
     proxy: {
